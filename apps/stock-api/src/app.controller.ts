36a58f4aab042500cbf8f38b2897e52ba038e067
Lalji Yadav <anubhavya246@gmail.com>
2025-11-21 17:59:17 +0530

    fix code issues

--- apps/stock-api/src/app.controller.ts
+++ apps/stock-api/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('analyze/:symbol')
+  analyzeStock(@Param('symbol') symbol: string) {
+   // return this.stocksService.analyzeStock(symbol);
+    return this.appService.analyzeStock(symbol)
+  }
+
 }
--- apps/stock-api/src/app.module.ts
+++ apps/stock-api/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FundamentalService } from './services/fundamental.service';
+import { ScoreService } from './services/score.service';
+import { SentimentService } from './services/sentiment.service';
+import { ValuationService } from './services/valuation.service';
+
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,FundamentalService,ScoreService,SentimentService,ValuationService],
 })
 export class AppModule {}
